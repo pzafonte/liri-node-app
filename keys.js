ada1a4fb1c06e68af33c08a8236009d09e211072
Peter David Zafonte <pzafonte@users.noreply.github.com>
2018-10-11 02:01:34 -0400

    minor fixes and generating log.txt

--- keys.js
+++ keys.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-console.log('this is loaded');
-
->>>>>>> a6c0dd6... initial upload of git ignore  environment variable set up and basic js
-=======
->>>>>>> 1d8acaf... liri-node-app v0.1
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
